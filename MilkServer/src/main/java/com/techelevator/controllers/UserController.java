514c81bac0b221b20631af4971167b04fcd4c4d2
Shane Killian <killias2@gmail.com>
2020-08-19 13:38:29 -0400

    Once again, have a working endpoint

--- MilkServer/src/main/java/com/techelevator/controllers/UserController.java
+++ MilkServer/src/main/java/com/techelevator/controllers/UserController.java
@@ -1,14 +1,27 @@
 package com.techelevator.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.model.User;
+import com.techelevator.model.UserDAO;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8081", allowedHeaders = "*")
 public class UserController {
 	
-	@RequestMapping("")
-	public String greeting() {
-		return "Hello World!";
+	private UserDAO userDAO;
+	
+	public UserController(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	public User addUser(@RequestBody User newUser) {
+		userDAO.addUser(newUser);
+		return newUser;
 	}
 
 }
